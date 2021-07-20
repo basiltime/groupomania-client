f776e58c96dc9d0ef31fea7303c0e390c5754e25
basiltime <krystalcanfield04@gmail.com>
2021-07-20 01:53:42 -0400

    Added SASS and began styling components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, {useState} from 'react';
+import './App.scss'
+import React from 'react';
 import logo from './logo.svg';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faUser)
 
 
-// {CreateNewAccountWrapper()}
-// {AccountWrapper()}
-// {CreatePostWrapper()}
-// {PostFeedWrapper()}
+
+
+
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function App() {
   return ( <Router>
-    
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-            
-            {Nav()}
-          
+    <div id="app"> 
+          {Nav()}
           <Switch>
           <Route exact path="/">
             <LoginWrapper />
@@ -33,105 +29,91 @@ function App() {
           <Route path="/create-account">
             <CreateNewAccountWrapper />
           </Route>
+          <Route path="/my-account">
+            <AccountWrapper />
+          </Route>
+          <Route path="/create-post">
+            <CreatePostWrapper />
+          </Route>
         </Switch>
         </div>
           </Router>
-           
   )
   }
-
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function Nav() {
-  return ( <nav  style={{border: "1px solid blue", display: "flex", flexDirection: "row", width: "30%"}}>
-    <img src={logo} alt="logo" style={{width: "55px"}}/>
-    <header><h1>Groupomania</h1></header>
+  return ( <nav>
+    <img src={logo} alt="logo" />
+    <Link to="/my-account"><FontAwesomeIcon icon="user" id="account-icon" color="white" /></Link>
   </nav>)
 }
+
+
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function LoginWrapper() {
-  return ( <div style={{border: "1px solid blue", width: "30%"}}>
+  return ( <main>
     {LoginForm()}
     {NewAccountButton()}
-  </div>)
-}
+  </main>)
+} 
 
 function LoginForm() {
-  return ( <form style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-    <label>
-      Email:
-      <input type="email" name="email" />
-    </label>
-    <label>
-      Password:
-      <input type="text" name="password" />
-    </label>
-    <Link to="/post-feed">Log In</Link>
- 
+  return ( <form>
+    <img src="/black-logo.svg" alt="logo" id="black-logo" />
+    <h2>Log in to your account</h2>
+      <input type="email" name="email" placeholder="Enter email address" aria-label="Enter email address"/>
+      <input type="text" name="password" placeholder="Enter your password" aria-label="Enter password"/>
+      <Link to="/post-feed" className="button-link">Log In</Link>
   </form>
   );
 }
 
 function NewAccountButton() {
-  return ( <div>       
+  return ( <div className="new-account-button">       
   <p>Or</p>
-  <Link to="/create-account">Create New Account</ Link>
+  <Link to="/create-account" className="button-link">Create New Account</ Link>
 </div>)
 };
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function CreateNewAccountWrapper() {
-  return ( <form style={{display: "flex", flexDirection: "column", alignItems: "flex-start", border: "1px solid blue", width: "30%"}}>
+function CreateNewAccountWrapper() { 
+  return ( <main>
+    <form>
     <h2>Enter your details to create a new account</h2>
-    <label>
-      Username:
-      <input type="text" name="username" />
-    </label>
-    <label>
-      Email:
-      <input type="email" name="email" />
-    </label>
-    <label>
-      Password:
-      <input type="text" name="password" />
-    </label>
-    <label>
-      Retype password:
-      <input type="text" name="retype-password" />
-    </label>
-    <label>
-      Choose Profile Pic:
-      <input type="file" name="upload-image" />
-    </label>
-    <input type="submit" value="Create Account" />
+      <input type="text" name="username" placeholder="Username" aria-label="username" />
+      <input type="email" name="email" placeholder="Email address" aria-label="email address" />
+      <input type="text" name="password" placeholder="Password" aria-label="password" />
+      <input type="text" name="retype-password" placeholder="Re-enter password" aria-label="enter password again" />
+      <label for="file-upload" className="button-link">Upload Profile Picture</label>
+      <input type="file" id="file-upload" aria-label="Upload profile picture"/>
+      <Link to="/post-feed" className="button-link">Create Account</Link>
   </form>
+  </main> 
   );
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function AccountWrapper() {
-  return ( <div style={{border: "1px solid blue", width: "30%"}}>
+  return ( <main>
     <AccountDetails username="basiltime" email="bob@evans.com" />
     <hr />
     {EditAccountDetails()}
     {DeleteAccount()}
     <hr />
     {LogOut()}
-  </div> )
+  </main> )
 }
 
 function AccountDetails(props) {
   return ( <div>
     <h2>Your Account</h2>
-    <img src={logo} alt="logo" style={{width: "200px"}} />
     <p>Username: {props.username}</p>
     <p>Email: {props.email}</p>
     <p>Password: xxxxxx</p>
   </div>)
 }
-
-
 
 function EditAccountDetails() {
   return ( <p>Edit Account Details</p> )
@@ -142,12 +124,12 @@ function DeleteAccount() {
 }
 
 function LogOut() {
-  return ( <button>Log Out</button> )
+  return ( <button className="button-link">Log Out</button> )
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function CreatePostWrapper() {
-  return ( <div style={{border: "1px solid blue", width: "30%"}}>
+  return ( <main>
     <h2>Create Post</h2>
     <label>
       Start typing...
@@ -157,51 +139,79 @@ function CreatePostWrapper() {
       Upload photo or video
       <input type="file" name="upload-media" />
     </label>
-  </div>)
+  </main>)
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function PostFeedWrapper() {
-  return ( <div style={{border: "1px solid blue", width: "30%"}}>
+  return ( <main>
+    {CreatePostLink()}
     <h2>This is where users can view posts</h2>
+    <hr />
+    <h3>Simple Post Placeholder</h3>
+  </main>)
+}
+
+function CreatePostLink() {
+  return ( <div>
+      <Link to="/create-post">Create Post</Link>
   </div>)
 }
-function PracticeComponent(props) {
-  return ( <div>
-    <p>"Great Job {props.name}, you passed some props! </p>
-    <button>Change My Color</button>
-    </div>)
-}
 
-function Example() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-
-
-
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 export default App;
 
 
-// // Original app before routing
-// function App() {
-//   return <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-//             <PracticeComponent name="Krystal" />
-//             {Example()}
-//             {Nav()}
-//             {LoginWrapper()}
-//             {CreateNewAccountWrapper()}
-//             {AccountWrapper()}
-//             {CreatePostWrapper()}
-//           </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// function PracticeComponent(props) {
+//   return ( <div>
+//     <p>"Great Job {props.name}, you passed some props! </p>
+//     <button>Change My Color</button>
+//     </div>)
+// }
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
 // }
