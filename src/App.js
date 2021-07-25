8eff4011812f133887a9c45038c8752c6dc6465f
basiltime <krystalcanfield04@gmail.com>
2021-07-25 13:36:48 -0400

    Renamed directory to Client

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(faUser, faUserCircle, faThumbsUp, faComment, faTrash, faEdit)
 ///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 function App() {
   return ( <Router>
     <div id="app"> 
@@ -36,29 +35,27 @@ function App() {
   )
 }
 ///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 function Nav() {
-  return ( <nav class="nav">
-    <img class="nav__logo" src={logo} alt="logo" />
+  return ( <nav className="nav">
+    <Link to="/"><img className="nav__logo" src={logo} alt="logo" /></Link>
     <Link className="nav__icon" to="/my-account"><FontAwesomeIcon icon="user" color="white" /></Link>
   </nav>)
 }
 ///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 function LandingPage() {
-  return ( <main class="main">
-    <img class="main__logo" src="/black-logo.svg" alt="logo" />
-    <h2 class="main__header">Log in to your account</h2>
+  return ( <main className="main">
+    <img className="main__logo" src="/black-logo.svg" alt="logo" />
+    <h2 className="main__header">Log in to your account</h2>
     {LoginForm()}
     <p>Or</p>
     {CreateNewAccountButton()}
   </main>)
 } 
 function LoginForm() {
-  return ( <form class="form">
-      <input class="form__input" type="email" name="email" placeholder="Enter email address" aria-label="Enter email address"/>
-      <input class="form__input" type="text" name="password" placeholder="Enter your password" aria-label="Enter password"/>
-      <Link to="/news-feed" className="button">Log In</Link>
+  return ( <form className="form">
+      <input className="form__input" type="email" name="email" placeholder="Enter email address" aria-label="Enter email address"/>
+      <input className="form__input" type="text" name="password" placeholder="Enter your password" aria-label="Enter password"/>
+      <Link to="/news-feed" className="button">Log In</ Link>
   </form>
   );
 }
@@ -69,68 +66,87 @@ function CreateNewAccountButton() {
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function CreateAccount() { 
-  return ( <main class="main">
-    <h2 class="main__header">Enter your details to create a new account</h2>
-    <form class="form">
-      <input class="form__input" type="text" name="username" placeholder="Username" aria-label="username" />
-      <input class="form__input" type="email" name="email" placeholder="Email address" aria-label="email address" />
-      <input class="form__input" type="text" name="password" placeholder="Password" aria-label="password" />
-      <input class="form__input" type="text" name="retype-password" placeholder="Re-enter password" aria-label="enter password again" />
-      <FontAwesomeIcon icon={faUserCircle} className="profile-pic" size="10x" />
+  return ( <main className="main">
+    <h2 className="main__header">Enter your details to create a new account</h2>
+    <form className="form">
+      <input className="form__input" type="text" name="username" placeholder="Username" aria-label="username" />
+      <input className="form__input" type="email" name="email" placeholder="Email address" aria-label="email address" />
+      <input className="form__input" type="text" name="password" placeholder="Password" aria-label="password" />
+      <input className="form__input" type="text" name="retype-password" placeholder="Re-enter password" aria-label="enter password again" />
+      <FontAwesomeIcon icon={faUserCircle} className="profile-pic" size="10x"/>
       <label className="button--small" for="file-upload">Upload Profile Picture</label>
-      <input class="form__input" type="file" id="file-upload" aria-label="Upload profile picture"/>
+      <input className="form__input" type="file" id="file-upload" aria-label="Upload profile picture"/>
       <Link to="/news-feed" className="button">Create Account</Link>
   </form>
   </main> 
   );
 }
 ///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 function AccountWrapper() {
-  return ( <main class="main">
-    <h2 class="main__header">Your Account</h2>
-    <hr class="hr"/> 
+  return ( <main className="main">
+    <h2 className="main__header">Your Account</h2>
+    <hr className="hr"/> 
     <AccountDetails username="basiltime" email="bob@evans.com" />
-    <hr class="hr" />
-    {DeleteAccount()}
-    <hr class="hr" />
+    <hr className="hr" />
+    {DeleteAccountButton()}
+    <hr className="hr" />
     {LogOut()}
+    {DeleteAccount()}
   </main> )
 }
 function AccountDetails(props) {
-  return ( <div class="account-details">
+  return ( <div className="account-details">
     
-      <p class="account-details__username">{props.username}</p>
-      <p class="account-details__password">{props.email}</p>
+      <p className="account-details__username">{props.username}</p>
+      <p className="account-details__password">{props.email}</p>
     
     <FontAwesomeIcon icon={faUserCircle} className="profile-pic" size="10x"/>
   </div>)
 }
-function DeleteAccount() {
+function DeleteAccountButton() {
   return ( <p>Delete Account&nbsp;&nbsp;<FontAwesomeIcon icon={faTrash} className={"trash-icon"} /></p> )
+}
+function DeleteAccount() {
+  return ( <div className="delete-account">
+    Are you sure you want to delete your Grouponania account?
+    <br />
+    <br />
+    <strong>This is permanent and cannot be undone.</strong>
+    <br />
+    <br />
+    <hr />
+    <br />
+    <div className="l-buttons">
+    <button className="button--small">Yes, delete my account</button>
+    <button className="button--small">Cancel</button>
+    </div>
+  </div>)
 }
 function LogOut() {
   return ( <button className="button">Log Out</button> )
 }
 ///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
 function CreatePost() {
-  return ( <main class="main create-post">
-    <form class="form">
-    <h2 class="main__header">Create Post</h2>
-    <hr class="hr" />
-    <textarea class="form__textarea" type="text" name="create-post" placeholder="Start typing..." aria-label="Create Post" />
+  return ( <main className="main create-post">
+    <form className="form">
+    <h2 className="main__header">Create Post</h2>
+    <hr className="hr" />
+    <textarea className="form__textarea" type="text" name="create-post" placeholder="Start typing..." aria-label="Create Post" />
     <button className="button--small">Upload Photo or Video</button>
     </form> 
-    <hr class="hr" />
+    <hr className="hr" />
     <button className="button">Create Post</button>
+    {PostCreated()}
   </main>)
 }
-///////////////////////////////////////////////////////////////////////////
+function PostCreated() {
+  return (<div className="alert__post-created">
+    <h2>Post Created!</h2></div>)
+}
 ///////////////////////////////////////////////////////////////////////////
 function Newsfeed() {
-  return ( <main class="main news-feed">
-    <Link to="/create-post" class="button--small">Create Post</Link>
+  return ( <main className="main news-feed">
+    <Link to="/create-post" className="button--small">Create Post</Link>
     {Post()}
     {Post()}
     {Post()}
@@ -139,30 +155,30 @@ function Newsfeed() {
   </main>)
 }
 function Post() {
-  return ( <div class="post">
-    <div class="post__heading">
+  return ( <div className="post">
+    <div className="post__heading">
       <FontAwesomeIcon icon="user-circle" className="profile-pic" color="black" size="2x" />
       <div>
-        <h3 class="post__author">username goes here</h3>
-        <p class="post__timestamp">timestamp goes here</p>
+        <h3 className="post__author">username goes here</h3>
+        <p className="post__timestamp">timestamp goes here</p>
       </div>
     </div>
 
-    <div class="post__body">
-      <img class="post__image"src="/sample-img.jpg" alt="sample" ></img>
-      <p class="post__text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
+    <div className="post__body">
+      <img className="post__image"src="/sample-img.jpg" alt="sample" ></img>
+      <p className="post__text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
       </p>
     </div>
 
-    <div class="comments-and-likes">
-      <p class="comments-and-likes__qty">1 like&nbsp;&nbsp;&nbsp; 1 comment</p>
-      <hr class="hr" />
-      <div class="icons__wrapper">
+    <div className="comments-and-likes">
+      <p className="comments-and-likes__qty">1 like&nbsp;&nbsp;&nbsp; 1 comment</p>
+      <hr className="hr" />
+      <div className="icons__wrapper">
         <FontAwesomeIcon icon={faThumbsUp} className={"post-icons"} />
         <FontAwesomeIcon icon={faComment} className={"post-icons"} />
       </div>
-      <hr class="hr" />
-      <div class="comment">
+      <hr className="hr" />
+      <div className="comment">
       <FontAwesomeIcon icon="user-circle" className="comment__author-profile-pic" />
       <div>Looks delicious! 😻
       </div>
@@ -171,7 +187,6 @@ function Post() {
   </div>)
 
 }
-///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 export default App;
 
