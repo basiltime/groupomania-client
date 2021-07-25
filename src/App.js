dd014548a59e606c89437335ec0e1dafcf587c38
basiltime <krystalcanfield04@gmail.com>
2021-07-24 21:10:28 -0400

    Renamed classes to utilize BEM naming convention. Changed favicon to SVG file type.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.scss'
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faUserCircle, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { faThumbsUp, faComment } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 library.add(faUser, faUserCircle, faThumbsUp, faComment, faTrash, faEdit)
-
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function App() {
@@ -18,73 +16,70 @@ function App() {
           {Nav()}
           <Switch>
           <Route exact path="/">
-            <LoginWrapper />
+            <LandingPage />
           </Route>
-          <Route path="/post-feed">
-            <PostFeedWrapper />
+          <Route path="/news-feed">
+            <Newsfeed />
           </Route>
           <Route path="/create-account">
-            <CreateNewAccountWrapper />
+            <CreateAccount />
           </Route>
           <Route path="/my-account">
             <AccountWrapper />
           </Route>
           <Route path="/create-post">
-            <CreatePostWrapper />
+            <CreatePost />
           </Route>
         </Switch>
         </div>
           </Router>
   )
-  }
+}
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 function Nav() {
   return ( <nav class="nav">
-    <img src={logo} alt="logo" class="logo__white" />
-    <Link to="/my-account"><FontAwesomeIcon icon="user" className="account-icon" color="white" /></Link>
+    <img class="nav__logo" src={logo} alt="logo" />
+    <Link className="nav__icon" to="/my-account"><FontAwesomeIcon icon="user" color="white" /></Link>
   </nav>)
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function LoginWrapper() {
+function LandingPage() {
   return ( <main class="main">
-    <img src="/black-logo.svg" alt="logo" class="logo__black" />
-    <h2 class="header">Log in to your account</h2>
+    <img class="main__logo" src="/black-logo.svg" alt="logo" />
+    <h2 class="main__header">Log in to your account</h2>
     {LoginForm()}
-    {NewAccountButton()}
+    <p>Or</p>
+    {CreateNewAccountButton()}
   </main>)
 } 
-
 function LoginForm() {
-  return ( <form>
-      <input type="email" name="email" placeholder="Enter email address" aria-label="Enter email address"/>
-      <input type="text" name="password" placeholder="Enter your password" aria-label="Enter password"/>
-      <Link to="/post-feed" className="button">Log In</Link>
+  return ( <form class="form">
+      <input class="form__input" type="email" name="email" placeholder="Enter email address" aria-label="Enter email address"/>
+      <input class="form__input" type="text" name="password" placeholder="Enter your password" aria-label="Enter password"/>
+      <Link to="/news-feed" className="button">Log In</Link>
   </form>
   );
 }
-
-function NewAccountButton() {
-  return ( <div className="new-account-button">      
-  <p>Or</p>
-  <Link to="/create-account" className="button-small">Create New Account</ Link>
-</div>)
+function CreateNewAccountButton() {
+  return ( <Link to="/create-account" className="button--small">Create New Account</ Link>
+)
 };
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function CreateNewAccountWrapper() { 
+function CreateAccount() { 
   return ( <main class="main">
-    <h2 class="header">Enter your details to create a new account</h2>
-    <form>
-      <input type="text" name="username" placeholder="Username" aria-label="username" />
-      <input type="email" name="email" placeholder="Email address" aria-label="email address" />
-      <input type="text" name="password" placeholder="Password" aria-label="password" />
-      <input type="text" name="retype-password" placeholder="Re-enter password" aria-label="enter password again" />
+    <h2 class="main__header">Enter your details to create a new account</h2>
+    <form class="form">
+      <input class="form__input" type="text" name="username" placeholder="Username" aria-label="username" />
+      <input class="form__input" type="email" name="email" placeholder="Email address" aria-label="email address" />
+      <input class="form__input" type="text" name="password" placeholder="Password" aria-label="password" />
+      <input class="form__input" type="text" name="retype-password" placeholder="Re-enter password" aria-label="enter password again" />
       <FontAwesomeIcon icon={faUserCircle} className="profile-pic" size="10x" />
-      <label for="file-upload" className="button-small">Upload Profile Picture</label>
-      <input type="file" id="file-upload" aria-label="Upload profile picture"/>
-      <Link to="/post-feed" className="button">Create Account</Link>
+      <label className="button--small" for="file-upload">Upload Profile Picture</label>
+      <input class="form__input" type="file" id="file-upload" aria-label="Upload profile picture"/>
+      <Link to="/news-feed" className="button">Create Account</Link>
   </form>
   </main> 
   );
@@ -93,94 +88,82 @@ function CreateNewAccountWrapper() {
 ///////////////////////////////////////////////////////////////////////////
 function AccountWrapper() {
   return ( <main class="main">
-    <h2 class="header">Your Account</h2>
-    <hr /> 
+    <h2 class="main__header">Your Account</h2>
+    <hr class="hr"/> 
     <AccountDetails username="basiltime" email="bob@evans.com" />
-    <hr />
-    {EditAccountDetails()}
+    <hr class="hr" />
     {DeleteAccount()}
-    <hr />
+    <hr class="hr" />
     {LogOut()}
   </main> )
 }
-
 function AccountDetails(props) {
-  return ( <div class="all-account-details">
-    <div class="account-details-minus-image">
-      <p class="username">{props.username}</p>
-      <p class="password">{props.email}</p>
-    </div>
+  return ( <div class="account-details">
+    
+      <p class="account-details__username">{props.username}</p>
+      <p class="account-details__password">{props.email}</p>
+    
     <FontAwesomeIcon icon={faUserCircle} className="profile-pic" size="10x"/>
   </div>)
 }
-
-function EditAccountDetails() {
-  return ( <p>Edit Account Details&nbsp;&nbsp;<FontAwesomeIcon icon={faEdit} className={"trash-icon"} /></p> )
-}
-
 function DeleteAccount() {
-  return ( <p>Delete Account&nbsp;&nbsp;<FontAwesomeIcon icon={faTrash} className={"edit-icon"} /></p> )
+  return ( <p>Delete Account&nbsp;&nbsp;<FontAwesomeIcon icon={faTrash} className={"trash-icon"} /></p> )
 }
-
 function LogOut() {
   return ( <button className="button">Log Out</button> )
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function CreatePostWrapper() {
+function CreatePost() {
   return ( <main class="main create-post">
-    <form>
-    <h2 class="header">Create Post</h2>
-    <hr />
-    <textarea type="text" name="create-post" placeholder="Start typing..." aria-label="Create Post" />
-    <button className="button-small">Upload Photo or Video</button>
+    <form class="form">
+    <h2 class="main__header">Create Post</h2>
+    <hr class="hr" />
+    <textarea class="form__textarea" type="text" name="create-post" placeholder="Start typing..." aria-label="Create Post" />
+    <button className="button--small">Upload Photo or Video</button>
     </form> 
-    <hr />
+    <hr class="hr" />
     <button className="button">Create Post</button>
   </main>)
 }
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-function PostFeedWrapper() {
+function Newsfeed() {
   return ( <main class="main news-feed">
-    <input type="text" id="create-post-input-fake" placeholder="Create Post" aria-label="Create Post" />
+    <Link to="/create-post" class="button--small">Create Post</Link>
     {Post()}
-    {CreatePostLink()}
+    {Post()}
+    {Post()}
+    {Post()}
+    {Post()}
   </main>)
 }
-
-function CreatePostLink() {
-  return ( <div>
-      <Link to="/create-post">Create Post</Link>
-  </div>)
-}
-
 function Post() {
   return ( <div class="post">
-    <div class="post-heading">
-      <FontAwesomeIcon icon="user-circle" className="author-profile-pic" color="black" size="2x" />
-      <div class="author-and-timestamp">
-        <h3>username goes here</h3>
-        <p>timestamp goes here</p>
+    <div class="post__heading">
+      <FontAwesomeIcon icon="user-circle" className="profile-pic" color="black" size="2x" />
+      <div>
+        <h3 class="post__author">username goes here</h3>
+        <p class="post__timestamp">timestamp goes here</p>
       </div>
     </div>
 
-    <div class="post-body">
-      <img src="/sample-img.jpg" alt="sample" class="post-body-image"></img>
-      <p class="post-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
+    <div class="post__body">
+      <img class="post__image"src="/sample-img.jpg" alt="sample" ></img>
+      <p class="post__text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
       </p>
     </div>
 
-    <div class="post-comments">
-      <p class="post-likes-and-comments-qty">1 like&nbsp;&nbsp;&nbsp; 1 comment</p>
-      <hr />
-      <div class="post-icons-wrapper">
+    <div class="comments-and-likes">
+      <p class="comments-and-likes__qty">1 like&nbsp;&nbsp;&nbsp; 1 comment</p>
+      <hr class="hr" />
+      <div class="icons__wrapper">
         <FontAwesomeIcon icon={faThumbsUp} className={"post-icons"} />
         <FontAwesomeIcon icon={faComment} className={"post-icons"} />
       </div>
-      <hr />
+      <hr class="hr" />
       <div class="comment">
-      <FontAwesomeIcon icon="user-circle" className="commenter-profile-pic" />
+      <FontAwesomeIcon icon="user-circle" className="comment__author-profile-pic" />
       <div>Looks delicious! 😻
       </div>
       </div>
