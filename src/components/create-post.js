b5fe1146573879d3afbd172efd6b67ec05cf7637
basiltime <krystalcanfield04@gmail.com>
2021-08-25 12:47:23 -0400

    Add error handling for create post form

--- src/components/create-post.js
+++ src/components/create-post.js
@@ -1,13 +1,15 @@
 import { useForm } from "react-hook-form"
 import { useHistory } from "react-router-dom"
+import { useState } from 'react'
 import axios from 'axios'
 
 
 function CreatePost() {
-    const { register, handleSubmit } = useForm();
-
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [error, setError] = useState(null)
     const history =  useHistory();
     const userId = localStorage.getItem("userId")
+    const token = localStorage.getItem("token")
 
     function onSubmit(data){
        let date = new Date().toLocaleDateString()
@@ -19,7 +21,12 @@ function CreatePost() {
         timestamp: timestamp,
         imageUrl: "https://images.unsplash.com/photo-1498805983167-a523078d762c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGlnaCUyMGZpdmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         userId: userId
-      })
+      }, {
+        headers: {
+          Authorization: token,
+        }
+      }
+      )
       
       .then(function (response) {
         console.log(response);
@@ -34,14 +41,18 @@ function CreatePost() {
     return ( <main className="main create-post">
       <form onSubmit={handleSubmit(onSubmit)} className="form">
       <h2 className="main__header">Create Post</h2>
+      {errors.textContent && (
+        <div role="alert" className="error">
+          Post cannot be empty
+        </div>
+      )}
       <hr className="hr" />
-      <textArea {...register("textContent", { required: true, minLength: 1})} className="form__textarea" type="text" name="textContent" placeholder="Start typing..." 
+      <textarea {...register("textContent", { required: true, minLength: 1})} className="form__textarea" type="text" name="textContent" placeholder="Start typing..." 
       aria-label="Create Post" />
       <button className="button--small">Upload Photo or Video</button>
       <hr className="hr" />
       <button className="button" type="submit">Create Post</button>
       </form> 
-      {/* {PostCreated()} */}
     </main>)
   }
 
