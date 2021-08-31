ec3490038aa3b1099c82250f5949537b6985b634
basiltime <krystalcanfield04@gmail.com>
2021-08-31 02:56:39 -0400

    Add more logic for displaying comments- still under construction.

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -5,40 +5,24 @@ import { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import '../App.scss'
 
-
-
-
 const Newsfeed = () => {
   const [posts, setPosts] = useState([])
-  const [comments, setComments] = useState([])
   const firstDisplay = posts.slice(0, 5)
   const [displayedPosts, setDisplayedPosts] = useState(firstDisplay)
   const [observedEl, setObservedEl] = useState(null)
 
   const fetchPosts = async () => {
-    let token = localStorage.getItem("token")
+    let token = localStorage.getItem('token')
     const posts = await axios('http://localhost:3000/posts', {
       headers: {
-        Authorization: token
-      }
+        Authorization: token,
+      },
     })
     setPosts(posts.data.data)
   }
 
-  const fetchComments = async () => {
-    let token = localStorage.getItem("token")
-    const getComments = await axios('http://localhost:3000/comments', {
-      headers: {
-        Authorization: token
-      }
-    })
-    setComments(getComments.data.data)
-    console.log(getComments)
-  }
-
   useEffect(() => {
     fetchPosts()
-    fetchComments()
   }, [])
 
   const loadMore = () => {
@@ -76,60 +60,91 @@ const Newsfeed = () => {
     }
   }, [observedEl, observer])
 
-  const history =  useHistory();
+  const history = useHistory()
 
-  function handleClick(){
-    history.push("/create-post")
+  function handleClick() {
+    history.push('/create-post')
   }
 
   return (
     <div class="news-feed">
-      <button className="button" onClick={handleClick}>Create Post</button>
-    <>
-      {displayedPosts.map((post) => (
-        
-        <div key={post.postId} className="post">
-          <div className="post__heading">
-            <img src="images/no-photo.png"
-              className="profile-pic"
-              alt="Profile Picure"
-            />
-            <div className="l-post-author+timestamp">
-              <h3 className="post__author">
-                {post.firstName} {post.lastName}
-              </h3>
-              <p className="post__timestamp">{post.timestamp}</p>
+      <button className="button" onClick={handleClick}>
+        Create Post
+      </button>
+      <>
+        {displayedPosts.map((post) => (
+          <div key={post.postId} className="post">
+            <div className="post__heading">
+              <img
+                src="images/no-photo.png"
+                className="profile-pic"
+                alt="Profile Picure"
+              />
+              <div className="l-post-author+timestamp">
+                <h3 className="post__author">
+                  {post.firstName} {post.lastName}
+                </h3>
+                <p className="post__timestamp">{post.timestamp}</p>
+              </div>
+            </div>
+            <div className="post__body">
+              <img
+                className="post__image"
+                src={post.imageUrl}
+                alt="sample"
+              ></img>
+              <p className="post__text-content">{post.textContent}</p>
+            </div>
+            <div className="comments-and-likes">
+              <p className="comments-and-likes__qty">
+                1 like&nbsp;&nbsp;&nbsp; 1 comment
+              </p>
+              <hr className="hr" />
+              <div className="icons__wrapper">
+                <FontAwesomeIcon icon={faThumbsUp} className={'post-icons'} />
+                <FontAwesomeIcon icon={faComment} className={'post-icons'} />
+              </div>
+              <hr className="hr" />
+              <Comment postId={post.postId} textContent={'butts'} />
             </div>
           </div>
-          <div className="post__body">
-            <img className="post__image" src={post.imageUrl} alt="sample"></img>
-            <p className="post__text-content">{post.textContent}</p>
-          </div>
-          <div className="comments-and-likes">
-            <p className="comments-and-likes__qty">
-              1 like&nbsp;&nbsp;&nbsp; 1 comment
-            </p>
-            <hr className="hr" />
-            <div className="icons__wrapper">
-              <FontAwesomeIcon icon={faThumbsUp} className={'post-icons'} />
-              <FontAwesomeIcon icon={faComment} className={'post-icons'} />
-            </div>
-            <hr className="hr" />
-            <div className="comment">
-            <img src="images/no-photo.png"
-              className="comment__author-profile-pic"
-              alt="Profile Picure"
-            />
-              <div>Hellooooooo</div>
-            </div>
-          </div>
-        </div>
-      ))}
+        ))}
 
-      {posts.length > displayedPosts.length && (
-        <p ref={setObservedEl}>LoadMore ...</p>
-      )}
-    </>
+        {posts.length > displayedPosts.length && (
+          <p ref={setObservedEl}>LoadMore ...</p>
+        )}
+      </>
+    </div>
+  )
+}
+
+function Comment(props) {
+  const [commentsList, setCommentsList] = useState([])
+  const fetchComments = async () => {
+    let token = localStorage.getItem('token')
+    const comments = await axios('http://localhost:3000/comments', {
+      headers: {
+        Authorization: token,
+      },
+    })
+    setCommentsList(comments.data.data)
+  }
+
+  useEffect(() => {
+    fetchComments()
+    console.log(commentsList)
+  }, [])
+
+  return (
+    <div className="comment">
+      <img
+        src="images/no-photo.png"
+        className="comment__author-profile-pic"
+        alt="Profile Picure"
+      />
+      {commentsList.map((comment) => (
+        <div>{comment.commentText}</div>
+      ))}
     </div>
   )
 }
