e96f5eed5174c71419300a6a442adc55030a5605
basiltime <krystalcanfield04@gmail.com>
2021-09-21 03:02:43 -0400

    Move no-photo.png out of images folder and into public folder

--- cypress/integration/0-groupomania-tests/E2E.js
+++ cypress/integration/0-groupomania-tests/E2E.js
@@ -1,6 +1,6 @@
 describe('End to End Test', () => {
   it('Can perform common user actions', () => {
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
 
     // Assert nav is displaying
     cy.get('.nav').should('be.visible')
@@ -19,19 +19,19 @@ describe('End to End Test', () => {
     cy.url().should('include', '/create-account')
     cy.get('.form').should('be.visible').find('.form__input').eq(0).type('John')
     cy.get('.form').find('.form__input').eq(1).type('Doe')
-    cy.get('.form').find('.form__input').eq(2).type('bbbbob@email.com')
+    cy.get('.form').find('.form__input').eq(2).type('11@email.com')
     cy.get('.form').find('.form__input').eq(3).type('1234567890')
     // ***** ADD ASSERTION FOR CHOOSE PROFILE PICTURE BUTTON ********
     cy.contains('Create Account').should('be.visible').click()
     cy.wait(1000)   
 
     // Navigates to newsfeed after account creation
+    cy.get('.newsfeed')
     cy.url().should('include', '/news-feed')
 
     // Go to Account Details Page
     cy.get('.nav__icon').click()
-    cy.url().should('include', '/my-account')
-    cy.wait(1000)
+    cy.get('.account-details').should('be.visible')
 
     // Log Out
     cy.contains('Log Out').should('be.visible').click()
@@ -39,7 +39,7 @@ describe('End to End Test', () => {
     cy.get('.main__header').should('have.text', 'Log in to your account')
 
     // Log Back In
-    cy.get('.form').should('be.visible').find('.form__input').eq(0).type('bbbbob@email.com')
+    cy.get('.form').should('be.visible').find('.form__input').eq(0).type('11@email.com')
     cy.get('.form').should('be.visible').find('.form__input').eq(1).type('1234567890')
     cy.contains('Log In').should('be.visible').click()
 
@@ -72,7 +72,7 @@ describe('End to End Test', () => {
 
 describe('Login/Create Account form Validation and Error Handling', () => {
   it('cannot log in with the incorrect password', () => {
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
     cy.contains('Log in to your account')
     cy.get('.form').find('.form__input').eq(0).type('test@user.com')
     cy.get('.form').find('.form__input').eq(1).type('1234567891') //
@@ -81,7 +81,7 @@ describe('Login/Create Account form Validation and Error Handling', () => {
   })
 
   it('cannot log in with an unregistered email', () => {
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
     cy.contains('Log in to your account')
     cy.get('.form').find('.form__input').eq(0).type('nonexistant@user.com')
     cy.get('.form').find('.form__input').eq(1).type('1234567891') //
@@ -90,11 +90,11 @@ describe('Login/Create Account form Validation and Error Handling', () => {
   })
 
   it('cannot log in if password or email is missing', () => {
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
     cy.get('.form').find('.form__input').eq(0).type('test@user.com')
     cy.contains('Log In').click()
     cy.contains('Password is required')
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
     cy.get('.form').find('.form__input').eq(1).type('1234567890')
     cy.contains('Log In').click()
     cy.contains('Email is required')
@@ -103,7 +103,7 @@ describe('Login/Create Account form Validation and Error Handling', () => {
 
 describe('create account error handling', () => {
   it('cannot create an account with a duplicate email', () => {
-    cy.visit('http://localhost:3006')
+    cy.visit('https://groupomania-site.netlify.app')
     cy.contains('Create New Account').click()
     cy.url().should('include', '/create-account')
     cy.get('.form').find('.form__input').eq(0).type('John')
